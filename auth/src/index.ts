79894030c0c10072babad9e5c511a5c7826b7ff0
muralir <muralidharan@twilightsoftwares.com>
2022-12-02 19:17:54 +0530

    feat: trest commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 const port = 5000
 app.get('*', (req, res) => {
-  res.send({ body: {} })
+   res.send({ body: {} })
 });
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`)
