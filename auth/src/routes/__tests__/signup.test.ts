3b08b74e4faa04101ea7649c53122f5c47001a72
muralir <muralidharan@twilightsoftwares.com>
2022-12-06 17:53:55 +0530

    create test

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -4,15 +4,90 @@ import app from '../../app';
 // it('Should return 405 for non-post requests to the signup route',async () => {
 
 // })
-it('Should return 422 if the email is not valid', async () => {
-	await request(app).post('/api/auth/signup').send({}).expect(422);
+/**
+ *  valid email conditions:
+ * 		- Standard await formats from 'express-validator' package
+ */
+describe('test Validity of email input', () => {
+	let password = '';
+	beforeAll(() => {
+		password = 'validPassword1'
+	})
+	it('should return 422 if the email is not provided', async () => {
+		await request(app).post('/api/auth/signup').send({ password }).expect(422);
+	})
+	it('Should return 422 if the email is not valid', async () => {
+		await request(app).post('/api/auth/signup').send({ password }).expect(422);
+	})
+	it('should return 200 if the email valid ', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email: 'test@yopmail.com', password })
+		.expect(200);
+	})
+})
+/**
+ *  valid email conditions:
+ * 		- At Least 8 charactors
+ * 		- At Most 32 Charactors
+ * 		- One lower-case letter
+ * 		- One upper-case letter
+ * 		- One Number
+ */
+describe('test Validity of password input', () => {
+	let email = '';
 
-	await request(app)
+	beforeAll(()=> {
+		email = 'test@yopmail.com'
+	})
+	it('should return 422 if the password is not provided', async () => {
+		await request(app)
 		.post('/api/auth/signup')
-		.send({ email: 'isInvalid' })
+		.send({ email})
 		.expect(422);
-	await request(app)
+	})
+
+	it('should return 422 if the password contains less than 8 charactors', async () => {
+		await request(app)
 		.post('/api/auth/signup')
-		.send({ password: 'isInvalid' })
+		.send({ email, password: 'valid12' })
 		.expect(422);
-});
+	})
+	it('should return 422 if the password contains more than 32 charactors', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'validvalidvalidvalidvalidvalidvalidvalidww' })
+		.expect(422);
+	})
+	it('should return 422 if the password contains One lower case letter', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'VALID12VALID12' })
+		.expect(422);
+	})
+	it('should return 422 if the password contains One upper case letter', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'valid12valid12valid12' })
+		.expect(422);
+	})
+	it('should return 422 if the password does not contain a number', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'validvalid' })
+		.expect(422);
+	})
+	it('should return 200 if the passord is valid', async () => {
+		await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'Smart@123' })
+		.expect(200);
+	})
+	it('should return 200 if the passord is valid', async () => {
+		const response = await request(app)
+		.post('/api/auth/signup')
+		.send({ email, password: 'validva' })
+		console.log(response.body);
+		
+	})
+})
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -8,12 +8,14 @@ const test = `http://www.unsocial.app/user/${myVariable}`;
 signupRouter.post(
 	'/api/auth/signup',
 	[body('email').isEmail().withMessage('Email must be a in valid format')],
+	[body('password').trim().isLength({ min: 8, max: 38 }).withMessage('Password must be between 8 to 32 charactors')],
+	[body('password').matches(/^(.*[a-z].*)$/).withMessage('Password should contain at least one lowercase letter')],
+	[body('password').matches(/^(.*[A-Z].*)$/).withMessage('Password should contain at least one Uppercase letter')],
+	[body('password').matches(/^(.*\d.*)$/).withMessage('Password should contain at least one digit')],
 	(req: Request, res: Response) => {
 		const errors = validationResult(req);
-		console.log('eroor', errors);
-
 		if (!errors.isEmpty()) {
-			res.status(422).send({});
+			res.status(422).send({errors: errors.array()});
 		}
 		res.send({});
 	}
